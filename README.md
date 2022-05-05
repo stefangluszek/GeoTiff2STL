25f98cd5a9795445764809c5b61aecbbd01f60ed
Stefan Gluszek <stefang@kahoot.com>
2022-05-05 17:54:11 +0200

    Script to convert GeoTiff to STL

--- README.md
+++ README.md
@@ -1,6 +1,15 @@
 # GeoTiff2STL
 
+## GeoTIFF data source
+[ghsl.jrc.ec.europa.eu](https://ghsl.jrc.ec.europa.eu/download.php?ds=pop)
+
 ## Dependencies
 [GDAL](https://gdal.org/index.html) and [Python GDAL bindings](https://gdal.org/api/python.html)
 
 On Ubuntu the easiest way to install both GDAL and python bindings is to install [gdal-bin](https://packages.ubuntu.com/bionic/gdal-bin) and [python-gdal](https://packages.ubuntu.com/bionic/gdal-bin).
+
+[numpy-stl](https://pypi.org/project/numpy-stl/)
+
+## Useful links
+1. [Geoprocessing with Python using Open Source GIS](https://www.gis.usu.edu/~chrisg/python/2009/)
+2. [AutoGIS course](https://automating-gis-processes.github.io/2016/index.html)
---
+++ gtiff2stl.py
@@ -0,0 +1,92 @@
+import sys
+import numpy
+from osgeo import gdal
+from stl import mesh
+
+base = 1
+population_scale = 640
+
+
+def make_cube(h):
+    # Define the 8 vertices of the cube
+    vertices = numpy.array([
+        [0, 0, 0],
+        [1, 0, 0],
+        [1, 1, 0],
+        [0, 1, 0],
+        [0, 0, h + base],
+        [1, 0, h + base],
+        [1, 1, h + base],
+        [0, 1, h + base]])
+    # Define the 12 triangles composing the cube
+    faces = numpy.array([
+        [0, 3, 1],
+        [1, 3, 2],
+        [0, 4, 7],
+        [0, 7, 3],
+        [4, 5, 6],
+        [4, 6, 7],
+        [5, 1, 2],
+        [5, 2, 6],
+        [2, 3, 6],
+        [3, 7, 6],
+        [0, 1, 5],
+        [0, 5, 4]])
+
+    # Create the mesh
+    cube = mesh.Mesh(numpy.zeros(faces.shape[0], dtype=mesh.Mesh.dtype))
+    for i, f in enumerate(faces):
+        for j in range(3):
+            cube.vectors[i][j] = vertices[f[j], :]
+
+    return cube
+
+
+if len(sys.argv) < 2:
+    print('Usage: {} <file.stl>'.format(sys.argv[0]))
+    exit(1)
+
+dataset = gdal.Open(sys.argv[1])
+
+print('Opening {}'.format(sys.argv[1]))
+
+if not dataset:
+    print('Failed to open: {}'.format(sys.argv[1]))
+    exit(1)
+
+print('Driver: {}/{}'.format(dataset.GetDriver().ShortName,
+      dataset.GetDriver().LongName))
+print('Size is: {} x {} x {}'.format(dataset.RasterXSize,
+      dataset.RasterYSize, dataset.RasterCount))
+
+band = dataset.GetRasterBand(1)
+
+data = band.ReadAsArray()  # data = gdal_array.LoadFile(sys.argv[1])
+nodata = band.GetNoDataValue()
+max_population = numpy.amax(data)
+
+print('Max population: {}'.format(max_population))
+
+# replace nodata wiht 0
+data[data == nodata] = 0
+
+# create a base
+data[data == 0] = base
+
+columns = len(data[0])
+rows = len(data)
+
+vertices = []
+for y in range(rows):
+    for x in range(columns):
+        h = int(data[y][x] / population_scale)
+        m = make_cube(h)
+        m.y += y
+        m.x += x
+        vertices.append(m.data.copy())
+
+m = mesh.Mesh(numpy.concatenate(vertices))
+
+m.save(sys.argv[1] + '.stl')
+
+dataset = None
